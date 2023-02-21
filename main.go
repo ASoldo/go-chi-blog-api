5f3a746ce8e10b7146b645d2708e3f8ef00bb353
Rootster <soldo.andrej@gmail.com>
2023-02-21 14:53:18 +0100

    Initialize project

---
+++ main.go
@@ -0,0 +1,155 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+type Post struct {
+	ID       int       `json:"id"`
+	Title    string    `json:"title"`
+	Content  string    `json:"content"`
+	Likes    int       `json:"likes"`
+	Comments []Comment `json:"comments"`
+}
+
+type Comment struct {
+	ID      int    `json:"id"`
+	Content string `json:"content"`
+}
+
+var (
+	posts     = make(map[int]Post)
+	postID    = 1
+	commentID = 1
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Get("/posts", getAllPosts)
+	r.Get("/posts/{id}", getPostByID)
+	r.Post("/posts", createPost)
+	r.Put("/posts/{id}", updatePostByID)
+	r.Delete("/posts/{id}", deletePostByID)
+	r.Post("/posts/{id}/like", likePostByID)
+	r.Post("/posts/{id}/comment", commentOnPost)
+
+	fmt.Println("Starting server on :8080")
+	http.ListenAndServe(":8080", r)
+}
+
+func getAllPosts(w http.ResponseWriter, r *http.Request) {
+	postsList := []Post{}
+	for _, post := range posts {
+		postsList = append(postsList, post)
+	}
+	json.NewEncoder(w).Encode(postsList)
+}
+
+func getPostByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post, ok := posts[id]
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(post)
+}
+
+func createPost(w http.ResponseWriter, r *http.Request) {
+	var post Post
+	if err := json.NewDecoder(r.Body).Decode(&post); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post.ID = postID
+	posts[post.ID] = post
+	postID++
+	json.NewEncoder(w).Encode(post)
+}
+
+func updatePostByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post, ok := posts[id]
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	var updatedPost Post
+	if err := json.NewDecoder(r.Body).Decode(&updatedPost); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post.Title = updatedPost.Title
+	post.Content = updatedPost.Content
+	posts[id] = post
+	json.NewEncoder(w).Encode(post)
+}
+
+func deletePostByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	if _, ok := posts[id]; !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	delete(posts, id)
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func likePostByID(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post, ok := posts[id]
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	post.Likes++
+	posts[id] = post
+	json.NewEncoder(w).Encode(post)
+}
+
+func commentOnPost(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	post, ok := posts[id]
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+	var comment Comment
+	if err := json.NewDecoder(r.Body).Decode(&comment); err != nil {
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		return
+	}
+	comment.ID = commentID
+	commentID++
+	post.Comments = append(post.Comments, comment)
+	posts[id] = post
+	json.NewEncoder(w).Encode(post)
+}
